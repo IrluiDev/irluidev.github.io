6ab007be4eee605326b3ddac7f7d06902e0c0094
lia.art <IrluiDev@users.noreply.github.com>
2021-02-16 20:23:18 -0430

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let viewportWidth = $(window).width();
-let viewportHeight = $(window).height();
+let viewportWidth = window.width();
+let viewportHeight = window.height();
 
 console.log(viewportWidth);
 console.log(viewportHeight); 
