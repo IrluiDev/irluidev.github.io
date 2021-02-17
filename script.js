2e05d554c618fd657311d87dd448220ea33b1d19
lia.art <IrluiDev@users.noreply.github.com>
2021-02-16 22:35:53 -0430

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ console.log(alto + "alto");
 let hola = "Hola";
 
 if (ancho >= 768 && alto >= 1024){
-    console.log(hola)
     if (ancho == 864 && alto == 1536){
         let imagen = document.getElementById("#imagenCentral");
         console.log(imagen +"/br" + "Aquí")
