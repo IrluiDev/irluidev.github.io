3952cd8c2ae9dc47b5dd6e1e56541495a383e151
lia.art <IrluiDev@users.noreply.github.com>
2021-02-16 21:21:27 -0430

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let imagenCabecera = "cabecera.jpg";
 let imagenPhone = "cabeceraPhone";
-
-console.log(imagenCabecera);
-alert(imagenPhone);
-
 let viewportWidth = window.screen.height;
 let viewportHeight = window.screen.width;
 
@@ -12,7 +8,7 @@ let alto = console.log(viewportHeight);
 
 if (ancho >= 768 && alto >= 1024){
     if (ancho == 864 && alto == 1536){
-
+        let imagen = document.getElementById("#imagenCentral");
         console.log("Hola")
     }
 }
