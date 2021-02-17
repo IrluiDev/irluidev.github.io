1de9f79fda18f1bb018f9ff73d7157f6fcb2baf1
lia.art <IrluiDev@users.noreply.github.com>
2021-02-17 15:26:14 -0430

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let ancho = window.screen.width;
 console.log(ancho,"/br", alto)
 let elemento = document.querySelector("#imagenCentral");
 
-if (alto >= 1366 && ancho >= 1024){
+if ((alto == 1366 || alto == 379) || (ancho == 1024 || ancho == 820)){
     let imagen = elemento.setAttribute("src", "cabeceraTablet.jpg");
-} else { if (alto >= 320 && ancho >= 1170){
+} else { if (alto >= 320 && ancho >= 1170 || ancho >= 820){
         let imagen = elemento.setAttribute("src", "cabecera.jpg");
     }    else{
-            if (alto >= 320 && ancho <= 820){
+            if (alto >= 320 && ancho <= 720){
                     let imagen = elemento.setAttribute("src", "cabeceraPhone.jpg");
                 }
             }
