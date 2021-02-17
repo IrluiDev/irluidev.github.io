c7a28fc863a1364aa283477d0dd4fc10a9d2e5f0
lia.art <IrluiDev@users.noreply.github.com>
2021-02-17 13:50:04 -0430

    portadas

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let ancho = window.screen.height;
-let alto = window.screen.width;
+let alto = window.screen.height;
+let ancho = window.screen.width;
+console.log(ancho,"/br", alto)
 let elemento = document.querySelector("#imagenCentral");
 
-if (ancho >= 768 && alto >= 1024){
-    if (ancho >= 864 && alto >= 1536){
+if (alto == 320 && ancho == 1170){
         let imagen = elemento.setAttribute("src", "cabecera.jpg");
-    } 
     }    else{
             let imagen = elemento.setAttribute("src", "cabeceraPhone2.jpg");
     }
