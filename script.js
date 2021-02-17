701dd74f4ad87433ccefa0d90b95dde0e6fca1ec
lia.art <IrluiDev@users.noreply.github.com>
2021-02-16 20:59:02 -0430

    jscript

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let viewportWidth = window.screen.height;
 let viewportHeight = window.screen.width;
 
-console.log(viewportWidth);
-console.log(viewportHeight); 
+let ancho = console.log(viewportWidth);
+let alto = console.log(viewportHeight); 
+
+if (ancho >= 768 && alto >= 1024){
+    if (ancho == 864 && alto == 1536){
+        let imagen = document.getElementById("#imagenCentral");
+        console.log(imagen)
+    }
+}
