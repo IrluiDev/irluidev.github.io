66d91ee3d2acc104b1043c78250267e4e368e976
lia.art <IrluiDev@users.noreply.github.com>
2021-02-16 21:29:57 -0430

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let viewportHeight = window.screen.width;
 
 let ancho = console.log(viewportWidth);
 let alto = console.log(viewportHeight); 
+let hola = "Hola";
 
 if (ancho >= 768 && alto >= 1024){
+    console.log(hola)
     if (ancho == 864 && alto == 1536){
         let imagen = document.getElementById("#imagenCentral");
         console.log("Hola")
